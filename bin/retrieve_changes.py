6c9556688c65a2f649966b5298680c057df4317c
ValMobYKang <yikai.kang@valtech.com>
2023-05-23 13:28:34 +0200

    update

---
+++ bin/retrieve_changes.py
@@ -0,0 +1,25 @@
+import logging
+
+def retrieve_changes(repo:str, version:str, token:str):
+    print(repo)
+    print(version)
+    print(token)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    parser = argparse.ArgumentParser(
+        "Retrieve Pull Requests and associated Work Items between the given tag and the previous one.\nOutput is a dict where the keys are Pull Request Ids and the values are lists of associated Work Item Ids."
+    )
+    parser.add_argument("--repo", help="Repository Name", required=True)
+    parser.add_argument(
+        "--version",
+        help="Current Tag from which the Pull Requests should be gathered for",
+        required=True,
+    )
+    parser.add_argument(
+        "--token",
+        help="Access token for Azure DevOps with permission to read Git and Work Items",
+        required=True,
+    )
+
+    data = retrieve_changes(**parser.parse_args().__dict__)
